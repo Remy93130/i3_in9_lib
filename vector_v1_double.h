d2dd1c732644b207cdc08b47c453e62a36a9051d
Remy93130 <remy93130@gmail.com>
2019-11-19 15:39:25 +0100

    Erreur dans l ennonce askip

--- vector_v1_double.h
+++ vector_v1_double.h
@@ -5,7 +5,7 @@ typedef struct
 {
 	size_t size;
 	double* elements;
-} struct_vector_v1_double;
+} ps_vector_v1_double;
 
 ps_vector_v1_double vector_v1_double_alloc(size_t n);
 
@@ -17,7 +17,7 @@ void vector_v1_double_insert(ps_vector_v1_double p_vector p_vector, size_t i, do
 
 void vector_v1_double_erase(ps_vector_v1_double p_vector, size_t i);
 
-void vector_v1_double_push_back(ps_vector_v1_double p_vector, p_object pobject);
+void vector_v1_double_push_back(ps_vector_v1_double p_vector,double v);
 
 void vector_v1_double_pop_back(ps_vector_v1_double p_vector);
 
