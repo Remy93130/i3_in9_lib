bafd9b75844d8142513f543716ab1a14ce0ec66f
Remy93130 <remy93130@gmail.com>
2020-01-07 16:33:52 +0100

    Fin des fonctions pour le bench

--- TP3/bench_vector_v1_double.c
+++ TP3/bench_vector_v1_double.c
@@ -4,6 +4,23 @@
 #include <stddef.h>
 #include <string.h>
 
+void _swap(p_s_vector_v1_double p_vector, size_t i)
+{
+    double index_value = get(p_vector, i);
+    vector_v1_double_set(p_vector, i, get(p_vector, i + 1));
+    vector_v1_double_set(p_vector, i + 1, index_value);
+}
+
+void _sort(p_s_vector_v1_double p_vector, size_t list_size)
+{
+    if (list_size == 1) return;
+    for (int i = 0; i < list_size - 1; i++) {
+        if (get(p_vector, i) > get(p_vector, i + 1))
+            _swap(p_vector, i);
+    }
+    _sort(p_vector, list_size - 1);
+}
+
 void insert_erase_random(p_s_vector_v1_double p_vector, size_t n)
 {
     for (size_t i = 0; i < n; ++i)
@@ -38,7 +55,37 @@ void read_write_random(p_s_vector_v1_double p_vector, size_t n)
 {
     for (size_t i = 0; i < n; ++i)
     {
-        /* code */
+        size_t rng = random_size_t(0, p_vector->size);
+        vector_v1_double_set(p_vector, rng, -1);
+    }
+    for (size_t i = 0; i < n; ++i)
+    {
+        size_t rng = random_size_t(0, p_vector->size);
+        get(p_vector, rng);
+    }
+}
+
+void read_write_sequential(p_s_vector_v1_double p_vector, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < p_vector->size; ++j)
+            vector_v1_double_set(p_vector, j, j);
+        for (size_t j = 0; j < p_vector->size; ++j)
+            get(p_vector, j);
+    }
+}
+
+void bubble_sort(p_s_vector_v1_double p_vector, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < p_vector->size; ++j)
+        {
+            size_t rng = random_size_t(0, 100);
+            vector_v1_double_set(p_vector, j, rng);
+        }
+        _sort(p_vector, p_vector->size);
     }
 }
 
@@ -65,4 +112,12 @@ int main(int argc, char const *argv[])
         insert_erase_head(vector, n);
     else if (0 == strcmp(argv[1], "insert_erase_tail"))
         insert_erase_tail(vector, n);
+    else if (0 == strcmp(argv[1], "read_write_random"))
+        read_write_random(vector, n);
+    else if (0 == strcmp(argv[1], "read_write_sequential"))
+        read_write_sequential(vector, n);
+    else if (0 == strcmp(argv[1], "bubble_sort"))
+        bubble_sort(vector, n);
+    else
+        printf("Test unknown\n");
 }
