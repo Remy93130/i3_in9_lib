c2c682f0c99b89a05f511825cd2dbf4afa51f29c
Remy93130 <remy93130@gmail.com>
2020-01-07 15:51:57 +0100

    on continue le tp3

--- TP3/bench_vector_v1_double.c
+++ TP3/bench_vector_v1_double.c
@@ -2,23 +2,67 @@
 #include "random.h"
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 
 void insert_erase_random(p_s_vector_v1_double p_vector, size_t n)
 {
-	
+    for (size_t i = 0; i < n; ++i)
+    {
+        size_t rng = random_size_t(0, p_vector->size);
+        vector_v1_double_insert(p_vector, rng, -1);
+    }
+    for (size_t i = 0; i < n; ++i)
+    {
+        size_t rng = random_size_t(0, p_vector->size);
+        vector_v1_double_erase(p_vector, rng);
+    }
+}
+
+void insert_erase_head(p_s_vector_v1_double p_vector, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+        vector_v1_double_insert(p_vector, 0, -1);
+    for (size_t i = 0; i < n; ++i)
+        vector_v1_double_erase(p_vector, 0);
+}
+
+void insert_erase_tail(p_s_vector_v1_double p_vector, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+        vector_v1_double_push_back(p_vector, -1);
+    for (size_t i = 0; i < n; ++i)
+        vector_v1_double_pop_back(p_vector);
+}
+
+void read_write_random(p_s_vector_v1_double p_vector, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        /* code */
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-	if (argc < 4)
-	{
-		printf("Usage: ./bench_vector_v1_double test_type init_size n\n");
-		return 0;
-	}
-	size_t init_size, n;
-	sscanf(argv[2], "%ld", &init_size);
-	sscanf(argv[3], "%ld", &n);
-	p_s_vector_v1_double vector = vector_v1_double_alloc(init_size);
-	for (int i = 0; i < init_size; ++i) 
-		vector_v1_double_set(vector, i, random_double(0, 100));
+    if (argc < 4)
+    {
+        printf("Usage: ./bench_vector_v1_double test_type init_size n\n");
+        return 0;
+    }
+    size_t init_size, n;
+
+    sscanf(argv[2], "%ld", &init_size);
+    sscanf(argv[3], "%ld", &n);
+
+    printf("\nStart bench\ntest: %s\ninit_size: %d\nn: %d\n", argv[1], init_size, n);
+
+    p_s_vector_v1_double vector = vector_v1_double_alloc(init_size);
+    for (int i = 0; i < init_size; ++i)
+        vector_v1_double_set(vector, i, random_double(0, 100));
+    if (0 == strcmp(argv[1], "insert_erase_random"))
+        insert_erase_random(vector, n);
+    else if (0 == strcmp(argv[1], "insert_erase_head"))
+        insert_erase_head(vector, n);
+    else if (0 == strcmp(argv[1], "insert_erase_tail"))
+        insert_erase_tail(vector, n);
 }
--- TP3/random.c
+++ TP3/random.c
@@ -1,43 +1,38 @@
 #include "random.h"
 #include <stdlib.h>
-#include <time.h>
 
 #ifndef __RANDOM__
 #define __RANDOM__
 
+// ((double)rand()) / (RAND_MAX + 1) => [0,1[
+
 double random_double(double a, double b)
 {
-	srand((unsigned)time(NULL));
 	return ((double)rand() * (b - a)) / (double)RAND_MAX + a;
 }
 
 float random_float(float a, float b)
 {
-	srand((unsigned)time(NULL));
 	return ((float)rand() * (b - a)) / (float)RAND_MAX + a;
 }
 
 size_t random_size_t(size_t a, size_t b)
 {
-	srand((unsigned)time(NULL));
 	return ((size_t)rand() * (b - a) / (size_t)RAND_MAX + a);
 }
 
 int random_int(int a, int b)
 {
-	srand((unsigned)time(NULL));
 	return (rand() * (b - a)) / RAND_MAX + a;	
 }
 
 unsigned char random_uchar(unsigned char a, unsigned char b)
 {
-	srand((unsigned)time(NULL));
 	return ((unsigned char)rand() * (b - a) / (unsigned char)RAND_MAX + a);
 }
 
 void random_init_string(unsigned char * c, size_t n)
 {
-	srand((unsigned)time(NULL));
 	for (size_t i = 0; i < n; i++) {
 		int rng = random_uchar('A', 'Z');
 		c[i] = rng;
